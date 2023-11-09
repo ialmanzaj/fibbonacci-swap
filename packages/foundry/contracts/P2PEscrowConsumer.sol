0e2c8204b3c942e399fd08242fe9fa34ab683497
Isaac Almanza <isaac.almanza19@gmail.com>
2023-11-09 14:01:39 -0500

    chore: remove console log

--- packages/foundry/contracts/P2PEscrowConsumer.sol
+++ packages/foundry/contracts/P2PEscrowConsumer.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/console.sol";
 import {ResultsConsumer} from "./ResultsConsumer.sol";
 import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @title
