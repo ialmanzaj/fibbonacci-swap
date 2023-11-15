468a8ab0c8b1ae71ff8fb63ea60ee3007c6c0a73
Isaac Almanza <isaac.almanza19@gmail.com>
2023-11-14 23:05:36 -0500

    feat: add validate payment endpoint with api-key protection

--- packages/nextjs/pages/api/validate_payment.ts
+++ packages/nextjs/pages/api/validate_payment.ts
@@ -1,23 +1,42 @@
-import { getAuthOptions } from "./auth/[...nextauth]";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withApiKey } from "~~/services/api-middleware";
 import { belvoClient } from "~~/services/belvo";
+import { db } from "~~/services/db";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const { amount, startedAt, taker, link, account } = req.body;
-    const current = new Date();
-    const finalAt = new Date();
-    const startedAtDate = current.toISOString().slice(0, 10);
-    const finalAtDate = finalAt.toISOString().slice(0, 10);
+const POST = withApiKey(async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method Not Allowed" });
+  }
 
-    current.setDate(current.getDate() - 1);
+  const { userId, amount, startedAt, taker, link, account } = req.body;
 
+  const newestDate = new Date(startedAt * 1000);
+  const newestDateStr = newestDate.toISOString().slice(0, 10);
+
+  const oldestDate = new Date();
+  oldestDate.setDate(newestDate.getDate() - 1);
+  const oldestDateStr = oldestDate.toISOString().slice(0, 10);
+
+  const user = await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  try {
     await belvoClient.connect();
-    const transactions = await belvoClient.transactions.retrieve(link, startedAtDate, finalAtDate, {
+
+    const transactions = await belvoClient.transactions.retrieve(link, oldestDateStr, newestDateStr, {
       saveData: false,
       account: account,
     });
+
     transactions.filter((transaction: any) => transaction.type === "OUTFLOW");
     res.json(transactions);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Belvo Server Error" });
   }
-};
+});
+
+export default POST;
