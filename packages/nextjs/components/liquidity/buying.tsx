896a23815dd7b03a416610e55cd0d0024955c2bb
Isaac Almanza <isaac.almanza19@gmail.com>
2023-11-18 17:19:20 -0500

    feat: change copy and add price per coin

--- packages/nextjs/components/liquidity/buying.tsx
+++ packages/nextjs/components/liquidity/buying.tsx
@@ -12,31 +12,34 @@ const BuyingSide: React.FC<BuyingSideProps> = ({ children, token, currency }) =>
     <div className="w-full flex flex-row gap-5 items-center">
       <div className="w-full flex flex-col gap-4">
         <div className="flex flex-row items-center justify-between">
-          <h2>Compras</h2>
+          <h2>Tu envias</h2>
           <TokenSymbol symbol={currency.symbol} name={currency.name} />
         </div>
         <input placeholder="Valor" type="number" className="input input-bordered bg-transparent w-full max-w-xs" />
       </div>
       <div className="w-full  flex flex-col gap-4">
         <div className="flex flex-row items-center justify-between">
-          <h2>Recibes</h2>
+          <h2>Tu Recibes</h2>
           <TokenSymbol symbol={token.symbol} name={token.name} />
         </div>
-        <input
-          type="number"
-          placeholder="Cantidad de Tokens "
-          className="input input-bordered bg-transparent w-full max-w-xs"
-        />
+        <input type="number" placeholder="" className="input input-bordered bg-transparent w-full max-w-xs" />
       </div>
     </div>
-
-    <div className="divider" />
-    <h2>Rango</h2>
-    <div className="flex flex-row  gap-4">
-      <input type="number" placeholder="Min" className="input input-bordered bg-transparent w-full max-w-xs" />
-      <input type="number" placeholder="Max" className="input input-bordered bg-transparent w-full max-w-xs" />
+    <div className="flex flex-row items-center justify-between">
+      <span>1 USDT</span>
+      <input type="number" placeholder="3,946.00 COP" className="input input-bordered bg-transparent w-full max-w-xs" />
     </div>
-    {children}
+    <div className="divider" />
+    <h2>Limites de compra</h2>
+    <div className="flex flex-row  gap-4">
+      <input type="number" placeholder="Minimo" className="input input-bordered bg-transparent w-full max-w-xs" />
+      <input type="number" placeholder="Maximo" className="input input-bordered bg-transparent w-full max-w-xs" />
+    </div>
+    <div className="divider" />
+    <div className="flex flex-row  gap-4">
+      <h2></h2>
+      {children}
+    </div>
     <button className="btn btn-ghost bg-indigo-600">Crear orden</button>
   </form>
 );
