df351b9a29ac1b1dc6b139e8d16bb95c365f2cca
Isaac Almanza <isaac.almanza19@gmail.com>
2023-11-21 17:16:48 -0500

    refactor: rename button to buy

--- packages/nextjs/components/home/Buying.tsx
+++ packages/nextjs/components/home/Buying.tsx
@@ -25,7 +25,7 @@ function Buying({ data }: any) {
       </div>
       <div className="text-sm leading-4 w-full">
         <button className="text-sm py-5 px-4 rounded-lg  focus:ring-2 bg-indigo-800 text-white duration-200 focus:ring-offset-2 focus:ring-white  w-full inline-flex items-center justify-center ring-1 ring-transparent">
-          Vender
+          Comprar
         </button>
       </div>
     </form>
