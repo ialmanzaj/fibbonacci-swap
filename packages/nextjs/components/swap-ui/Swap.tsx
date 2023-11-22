125281077cff469a6b9c3cbe8dafaeaaa764ff80
Isaac Almanza <isaac.almanza19@gmail.com>
2023-11-22 16:00:44 -0500

    feat: add swap ui interaction

---
+++ packages/nextjs/components/swap-ui/Swap.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Buying } from "../home/Buying";
+import { Selling } from "../home/Selling";
+import { useDeployedContractInfo, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+
+const Swap = ({ orders }) => {
+  const [isSelling, setIsSelling] = useState(false);
+
+  const { data: activeEscrows } = useScaffoldContractRead({
+    contractName: "FibbonacciEscrow",
+    functionName: "activeEscrows",
+    args: [],
+    watch: true,
+  });
+  console.log(activeEscrows);
+
+  return (
+    <div className="flex relative pb-10 bg-black">
+      <main className="relative my-auto justify-center w-full px-5 flex flex-col items-center">
+        <div className="relative flex flex-col gap-5 mb-10">
+          <ul className="menu w-full  bg-[#18181B]  menu-horizontal  text-white font-bold rounded-lg">
+            <li className=" w-1/2">
+              <button
+                disabled={!isSelling}
+                onClick={() => setIsSelling(!isSelling)}
+                className={`${!isSelling ? "bg-indigo-800 cursor-default" : "bg-auto"} w-full  flex justify-center`}
+              >
+                Compra
+              </button>
+            </li>
+
+            <li className="w-1/2">
+              <button
+                disabled={isSelling}
+                onClick={() => setIsSelling(!isSelling)}
+                className={`${
+                  isSelling ? "bg-indigo-800 cursor-default" : "bg-auto cursor-pointer"
+                } w-full   flex justify-center`}
+              >
+                Vende
+              </button>
+            </li>
+          </ul>
+          {isSelling ? <Selling orders={orders} /> : <Buying />}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Swap;
